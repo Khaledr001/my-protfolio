6c7d4bb7b8e035f55b20cb04e8e24dd2800846ba
Khaledr001 <khaledrazu01@gmail.com>
2024-03-27 04:19:13 +0600

    daisyui deleted and css variable added for theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import './App.css'
-import Navbar from './components/nav'
-import Projects from './components/projects'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./components/nav";
+import Projects from "./components/projects";
 
 function App() {
 
+  const [theme, setTheme] = useState<string>(
+    localStorage.getItem("theme") ? localStorage.getItem("theme")! : "light"
+  ); 
+
+  // const handleToggle = (e: ChangeEvent<HTMLInputElement>): void => {
+  //   if (e.target.checked) setTheme("dark");
+  //   else setTheme("light");
+  // };
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme") ?? "light";
+    document.querySelector("html")?.setAttribute("data-theme", localTheme);
+  }, [theme]);
+
   return (
     <>
-      <Navbar/>
-      <h1 className='font-poppins'>Hello Khaled</h1>
-      <Projects />
+      <div className={`background-container ${theme}`}>
+        <Navbar />
+        <Projects />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,15 +1,6 @@
-import React from "react";
 
 const Projects = () => {
-  return (
-    <>
-      <div className="md:flex ">
-        <div className="h-44 w-72 bg-red-500 md:flex-grow-0"></div>
-        <div className="h-44 w-72 bg-yellow-500"></div>
-        <div className="h-44 w-72 bg-green-400"></div>
-      </div>
-    </>
-  );
+  return <div className="h-44 w-44 bg-secondary">hi</div>;
 };
 
 export default Projects;
