317007daffd5fb86c62938b3af6445b8ba8b7d58
unknown <khaledrazu01@gmail.com>
2024-08-20 21:20:50 +0600

    something

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Navbar from "./components/nav";
-import Projects from "./components/projects";
+// import Home from "./pages/home";
+import Design from "./components/particle";
+import Home from "./pages/home";
+import Modal from "./components/Modal";
 
 function App() {
-
   const [theme, setTheme] = useState<string>(
     localStorage.getItem("theme") ? localStorage.getItem("theme")! : "light"
-  ); 
+  );
 
   // const handleToggle = (e: ChangeEvent<HTMLInputElement>): void => {
   //   if (e.target.checked) setTheme("dark");
@@ -22,9 +23,28 @@ function App() {
 
   return (
     <>
-      <div className={`background-container ${theme}`}>
-        <Navbar />
-        <Projects />
+      <div>
+        <div
+          className={`background-container ${theme} w-full h-full bg-banner-bg bg-no-repeat bg-center bg-cover overflow-hidden`}>
+          {/* <Navbar />
+        <Projects /> */}
+          {/* <canvas className="  h-full w-full z-[-1]"> */}
+          {/* <div className="relative"> */}
+
+          <div className="relative w-full h-full">
+            <div className="w-full h-screen  top-0 left-0 bottom-0 right-0 absolute">
+              <Design />
+            </div>
+          </div>
+          <div className=" mx-auto text-white ">
+            <Home />
+          </div>
+        </div>
+        {/* </div> */}
+        {/* </canvas> */}
+        <div id="hello">
+          <Modal />
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "banner-bg":"url('/public/assets/images/bannerBg.jpg')"
+      },
       colors: {
         primary: "rgb(var(--color-primary) / <alpha-value>)",
         secondary: "rgb(var(--color-secondary) / <alpha-value>)",
